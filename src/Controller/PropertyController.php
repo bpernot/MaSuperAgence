d219bf6a769d459433e998cfd0135abfb2d390ab
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-05-05 16:41:29 +0200

    push

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+  /**
+   * @var PropertyRepository
+   */
+  private $repository;
+
+  /**
+   * @var ObjectManager
+   */
+  private $em;
+
+  public function __construct(PropertyRepository $repository, ObjectManager $em)
+  {
+    $this->repository = $repository;
+    $this->em = $em;
+  }
+
   /**
    * index
    * @Route("/biens", name="property.index")
