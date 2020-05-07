9086c01e2adec73b3bccfc00e5dc0a462d14f2f9
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-05-08 01:59:03 +0200

    add: Pagination

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -6,8 +6,10 @@ use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class PropertyController extends AbstractController
 {
@@ -32,10 +34,16 @@ class PropertyController extends AbstractController
    * @Route("/biens", name="property.index")
    * @return Response
    */
-  public function index(): Response
+  public function index(PaginatorInterface $paginator, Request $request): Response
   {
+    $properties = $paginator->paginate(
+      $this->repository->findAllVisibleQuery(),
+      $request->query->getInt('page', 1),
+      12
+    );
     return $this->render('property/index.html.twig', [
-      'current_menu' => 'properties'
+      'current_menu' => 'properties',
+      'properties' => $properties
     ]);
   }
   /**
